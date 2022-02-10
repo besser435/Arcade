923b8ade46ebaf795c259a44d3583ecc5bd43ef1
Brandon <68145204+besser435@users.noreply.github.com>
2022-02-09 19:34:45 -0700

    Update Keypad Test.py

--- Feather Arcade/Keypad Test.py
+++ Feather Arcade/Keypad Test.py
@@ -1,6 +1,3 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
 import time
 import digitalio
 import board
@@ -15,10 +12,9 @@ led_neo = neopixel.NeoPixel(board.NEOPIXEL, 1)
 # Use the same wiring as in the guide with the following setup lines:
 cols = [digitalio.DigitalInOut(x) for x in (board.D11, board.D13, board.D9)]
 rows = [digitalio.DigitalInOut(x) for x in (board.D12, board.D5, board.D6, board.D10)]
-
 keys = ((1, 2, 3), (4, 5, 6), (7, 8, 9), ("*", 0, "#"))
-
 keypad = adafruit_matrixkeypad.Matrix_Keypad(rows, cols, keys)
+
 
 """while True:
     keys = keypad.pressed_keys
